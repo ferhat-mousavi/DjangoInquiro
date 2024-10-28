69cea6f244ce3630c6e16e33b6da25d403e8db3a
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2024-10-29 02:31:52 +0300

    Home page added.

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from questions.models import Question
 
-# Create your views here.
+
+class QuestionListView(ListView):
+    model = Question
+    template_name = 'home/question_list.html'  # Template file path
+    context_object_name = 'questions'  # Context variable name for the template
+    ordering = ['-created_at']  # Order questions by creation date (newest first)
+    paginate_by = 10  # Number of questions per page
