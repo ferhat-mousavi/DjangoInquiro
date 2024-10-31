76b6cbee147b4ba7d1a546e992184d5db27edcde
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2024-10-31 20:35:50 +0300

    bootstrap added to answer form.

--- answers/forms.py
+++ answers/forms.py
@@ -7,3 +7,10 @@ class AnswerForm(forms.ModelForm):
     class Meta:
         model = Answer
         fields = ['content']  # Only content field for the answer
+        widgets = {
+            'content': forms.Textarea(
+                attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Type your answer here...'}),
+        }
+        labels = {
+            'content': 'Your Answer',
+        }
