c2437f3abae593fbfa62d5e0f67cd3385e404589
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2024-10-31 23:58:13 +0300

    voting added to answers of question detail view.

--- answers/urls.py
+++ answers/urls.py
@@ -1,7 +1,8 @@
 # answers/urls.py
 from django.urls import path
-from .views import WriteAnswerView
+from .views import WriteAnswerView, AnswerVoteView
 
 urlpatterns = [
     path('write/<int:question_id>/', WriteAnswerView.as_view(), name='write_answer'),
+    path('vote/<int:id>/<str:vote_type>/', AnswerVoteView.as_view(), name='answer_vote'),
 ]
--- answers/views.py
+++ answers/views.py
@@ -1,9 +1,12 @@
 # answers/views.py
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.http import require_POST
 from questions.models import Question
 from .models import Answer
 from .forms import AnswerForm
+
 
 class WriteAnswerView(View):
     def get(self, request, question_id):
@@ -21,3 +24,18 @@ class WriteAnswerView(View):
             answer.save()
             return redirect('question_detail', slug=question.slug)
         return render(request, 'answers/write_answer.html', {'question': question, 'form': form})
+
+
+@method_decorator(require_POST, name='dispatch')
+class AnswerVoteView(View):
+    def post(self, request, id, vote_type):
+        answer = get_object_or_404(Answer, id=id)
+
+        if vote_type == 'up':
+            answer.up_vote()
+        elif vote_type == 'down':
+            answer.down_vote()
+
+        answer.save()
+        # Redirect to the question detail page where the answer is displayed
+        return redirect('question_detail', slug=answer.question.slug)
