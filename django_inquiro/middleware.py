9d19ea9a4b060859cbf294ebbf7c20e800ddd67e
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2024-11-25 23:06:37 +0300

    Django Admin Excluded from minifier.

--- django_inquiro/middleware.py
+++ django_inquiro/middleware.py
@@ -5,8 +5,17 @@ class HTMLMinifyMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
+        # Define URLs to exclude from processing
+        self.excluded_urls = [
+            '/admin/',  # Django Admin
+        ]
+
     def __call__(self, request):
         response = self.get_response(request)
+
+        # Skip processing for certain URLs
+        if any(request.path.startswith(url) for url in self.excluded_urls):
+            return response
 
         # Only minify HTML content
         if 'text/html' in response['Content-Type']:
@@ -15,6 +24,7 @@ class HTMLMinifyMiddleware:
                 remove_comments=True,
                 remove_empty_space=True,
                 remove_all_empty_space=True,
-                reduce_boolean_attributes=True).encode('utf-8')
+                reduce_boolean_attributes=True
+            ).encode('utf-8')
 
         return response
